db7e2591e5d6054bcc3c77766706b97fded46a30
Maicol Moretti <maicol.moretti11@gmail.com>
2020-04-04 15:00:17 +0200

    Add files via upload

--- BlueFound/BlueFound/Main_Page.vb
+++ BlueFound/BlueFound/Main_Page.vb
@@ -1,26 +1,32 @@
-﻿Imports Microsoft.Office.Interop
+﻿'importo la libreria che mi serve per lavorare con Word
+Imports videoscrittura = Microsoft.Office.Interop.Word
+
 
 Public Class Main_Page
 
     'Funzione che mi permette di uscire dal mio programma
-    '*
-    '*
-    'inserire una funzione di controllo con MsgBox per la conferma
+
     Private Sub Esci_Click(sender As Object, e As EventArgs) Handles Esci.Click
         Me.Hide()
         Schermata_di_Login.Show()
     End Sub
 
-    Private Sub apri_word_Click(sender As Object, e As EventArgs) Handles apri_word.Click
-        Dim variabileWord As Word.Application
-        variabileWord = CreateObject("WORD.APPLICATION")
-        variabileWord.Visible = True
-        variabileWord.Documents.Add()
-        variabileWord.Selection.PasteSpecial(Link:=2)
 
+    'Creo una unzione per l'apertura di un nuovo file Microdoft Word
+    Private Sub apri_word_Click(sender As Object, e As EventArgs) Handles apri_word.Click
+        Dim programma As videoscrittura.Application
+        Dim documento As videoscrittura.Document
+        programma = CreateObject("Word.Application")
+        programma.Visible = True
+        programma.Documents.Add()
+        documento = programma.Documents.Add
+
+        documento.Range.Text = messaggio.Text
 
     End Sub
 
+    'Questa funzione mi serve per vedere se il programma riesce a leggere la stringa che scrivo nel "messaggio" 
+    ' --> fa comparire un "Avvertimento" che contiene la stringa inserita
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim mioMessaggio As String = messaggio.Text
         MessageBox.Show(messaggio.Text)
